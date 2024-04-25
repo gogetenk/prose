3e6077ae1e47176fe39f00bc0ce65107a105a2f3
Yannis TOCREAU <Yannis.TOCREAU@consulting-for.edenred.com>
2024-04-25 11:01:03 +0200

    fix: aspire fix

--- Processia.Prose/Processia.Prose.Api/Controllers/PersonalityTestsController.cs
+++ Processia.Prose/Processia.Prose.Api/Controllers/PersonalityTestsController.cs
@@ -1,48 +1,48 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
+﻿//using MediatR;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Processia.Prose.Api.Controllers;
+//namespace Processia.Prose.Api.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class PersonalityTestsController : ControllerBase
-{
-    private readonly IMediator _mediator;
-    public PersonalityTestsController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
+//[ApiController]
+//[Route("api/[controller]")]
+//public class PersonalityTestsController : ControllerBase
+//{
+//    private readonly IMediator _mediator;
+//    public PersonalityTestsController(IMediator mediator)
+//    {
+//        _mediator = mediator;
+//    }
 
-    [HttpGet("start")]
-    public async Task<ActionResult<StartPersonalityTestDto>> StartTest()
-    {
-        var query = new StartPersonalityTestQuery();
-        var result = await _mediator.Send(query);
-        return Ok(result);
-    }
+//    [HttpGet("start")]
+//    public async Task<ActionResult<StartPersonalityTestDto>> StartTest()
+//    {
+//        var query = new StartPersonalityTestQuery();
+//        var result = await _mediator.Send(query);
+//        return Ok(result);
+//    }
 
-    [HttpPost("submit")]
-    public async Task<ActionResult<NextPersonalityQuestionDto>> SubmitAnswer(SubmitPersonalityTestAnswerDto dto)
-    {
-        var command = new SubmitPersonalityTestAnswerCommand(dto.QuestionId, dto.Answer);
-        var result = await _mediator.Send(command);
-        return Ok(result);
-    }
+//    [HttpPost("submit")]
+//    public async Task<ActionResult<NextPersonalityQuestionDto>> SubmitAnswer(SubmitPersonalityTestAnswerDto dto)
+//    {
+//        var command = new SubmitPersonalityTestAnswerCommand(dto.QuestionId, dto.Answer);
+//        var result = await _mediator.Send(command);
+//        return Ok(result);
+//    }
 
-    [HttpPost("results")]
-    public async Task<IActionResult> SaveResults(SavePersonalityTestResultsDto dto)
-    {
-        var command = new SavePersonalityTestResultsCommand(dto.UserId, new PersonalityTestResult(dto.Type, dto.Traits));
-        await _mediator.Send(command);
-        return Ok();
-    }
-}
+//    [HttpPost("results")]
+//    public async Task<IActionResult> SaveResults(SavePersonalityTestResultsDto dto)
+//    {
+//        var command = new SavePersonalityTestResultsCommand(dto.UserId, new PersonalityTestResult(dto.Type, dto.Traits));
+//        await _mediator.Send(command);
+//        return Ok();
+//    }
+//}
 
-// Request and Response records
-public record StartPersonalityTestDto(string Question, string[] Options);
+//// Request and Response records
+//public record StartPersonalityTestDto(string Question, string[] Options);
 
-public record SubmitPersonalityTestAnswerDto(Guid QuestionId, string Answer);
+//public record SubmitPersonalityTestAnswerDto(Guid QuestionId, string Answer);
 
-public record NextPersonalityQuestionDto(Guid NextQuestionId, string NextQuestion, string[] Options);
+//public record NextPersonalityQuestionDto(Guid NextQuestionId, string NextQuestion, string[] Options);
 
-public record SavePersonalityTestResultsDto(Guid UserId, string Type, string[] Traits);
+//public record SavePersonalityTestResultsDto(Guid UserId, string Type, string[] Traits);
---
+++ Processia.Prose/Processia.Prose.Application/Interfaces/IPostRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Processia.Prose.Application.Domain.Entities;
+
+namespace Processia.Prose.Application.Interfaces;
+
+public interface IPostRepository
+{
+    Task AddAsync(Post post, CancellationToken cancellationToken);
+}
